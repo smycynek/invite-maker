1c89d3ae086016035981ce5911b94f9d3e8a3a8b
smycynek <sv@stevenvictor.net>
2026-03-21 07:57:46 -0400

    improve clean script

---
+++ scripts/clean.sh
@@ -0,0 +1,11 @@
+
+#! /bin/bash
+
+rm -f invite.txt invite.jpg invite.zip invitation.jpg public/*.jpg public/preview.png 
+rm -f src/res/film1.json src/res/film2.json src/res/date-time.json src/res/general.json
+rm -rf invite
+git checkout scripts/sitevars.sh
+git checkout invitedata/general.json
+git checkout invitedata/date-time.json
+git checkout index.html
+git checkout src/fontandcolor.css
