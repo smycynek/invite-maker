897e6c052f77fcd8438747d0ab57d29402393a21
smycynek <sv@stevenvictor.net>
2026-03-23 18:23:37 -0400

    remove eslint/solidjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
-import solidPlugin from 'eslint-plugin-solid';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 import globals from 'globals';
@@ -26,17 +25,12 @@ export default [
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
-      solid: solidPlugin,
       prettier: prettierPlugin,
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
-      ...solidPlugin.configs.recommended.rules,
       'prettier/prettier': 'error',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'solid/reactivity': 'warn',
-      'solid/no-destructure': 'warn',
-      'solid/jsx-no-undef': 'error',
     },
   },
   prettierConfig,
